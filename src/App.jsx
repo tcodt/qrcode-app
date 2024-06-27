c7cefcfc86bf3b53d65f773cd50b509605dac216
TCOD <tcoddev@gmail.com>
2024-06-27 15:02:38 +0330

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [temp, setTemp] = useState("");
+  const [word, setWord] = useState("");
+  const [size, setSize] = useState(400);
+  const [bgColor, setBgColor] = useState("ffff");
+  const [qrcode, setQrcode] = useState("");
+
+  return (
+    <>
+      <div className="container">
+        <h1>QR Code Generator</h1>
+        <div className="input-box">
+          <input type="text" placeholder="متن خود را وارد کنید" />
+          <button>تولید کن</button>
+        </div>
+        <div className="extra">
+          <div>
+            <h5>رنگ پیش زمینه:</h5>
+            <input type="color" />
+          </div>
+          <div>
+            <h5>ابعاد:</h5>
+            <input type="range" min={200} max={600} />
+          </div>
+        </div>
+        <div className="output-box">
+          <img src={qrcode} alt="generated qrcode" />
+        </div>
+        <div className="download-box">
+          <a href={qrcode} download="QRCode">
+            <button className="button">دانلود</button>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
