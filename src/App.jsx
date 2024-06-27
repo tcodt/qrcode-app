e3ee5e4bd2eebdc710d5121a7b6244c3cf97f6d3
TCOD <tcoddev@gmail.com>
2024-06-27 16:11:37 +0330

    add project logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -8,31 +8,56 @@ function App() {
   const [bgColor, setBgColor] = useState("ffff");
   const [qrcode, setQrcode] = useState("");
 
+  useEffect(() => {
+    setQrcode(
+      `http://api.qrserver.com/v1/create-qr-code/?data=${word}&size=${size}x${size}&bgcolor=${bgColor}`
+    );
+  }, [word, size, bgColor]);
+
+  const handleClick = () => {
+    setWord(temp);
+  };
+
   return (
     <>
-      <div className="container">
-        <h1>QR Code Generator</h1>
-        <div className="input-box">
-          <input type="text" placeholder="متن خود را وارد کنید" />
-          <button>تولید کن</button>
-        </div>
-        <div className="extra">
-          <div className="bg-color">
-            <h5>رنگ پیش زمینه:</h5>
-            <input type="color" />
+      <div className="app">
+        <div className="container">
+          <h1>QR Code Generator</h1>
+          <div className="input-box">
+            <input
+              type="text"
+              placeholder="متن خود را وارد کنید"
+              onChange={(e) => setTemp(e.target.value)}
+            />
+            <button onClick={handleClick}>تولید کن</button>
           </div>
-          <div className="size">
-            <h5>ابعاد:</h5>
-            <input type="range" min={200} max={600} />
+          <div className="extra">
+            <div className="bg-color">
+              <h5>رنگ پیش زمینه:</h5>
+              <input
+                type="color"
+                onChange={(e) => setBgColor(e.target.value.substring(1))}
+              />
+            </div>
+            <div className="size">
+              <h5>ابعاد:</h5>
+              <input
+                type="range"
+                min={200}
+                max={600}
+                value={size}
+                onChange={(e) => setSize(e.target.value)}
+              />
+            </div>
           </div>
-        </div>
-        <div className="output-box">
-          <img src={qrcode} alt="generated qrcode" />
-        </div>
-        <div className="download-box">
-          <a href={qrcode} download="QRCode">
-            <button className="button">دانلود</button>
-          </a>
+          <div className="output-box">
+            <img src={qrcode} alt="generated qrcode" />
+          </div>
+          <div className="download-box">
+            <a href={qrcode} download="QRCode">
+              <button className="button">دانلود</button>
+            </a>
+          </div>
         </div>
       </div>
     </>
